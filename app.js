417c6effc3e3289f92fd982a1d85748d8e4c6f55
conorbailey90 <conbailey90@gmail.com>
2021-08-01 15:41:19 +0100

    Initial commit

---
+++ app.js
@@ -0,0 +1,105 @@
+const scrollable = document.querySelector('.scrollable');
+const heroSection = document.getElementById('hero');
+const projectsSection = document.getElementById('projects');
+const aboutSection = document.getElementById('about');
+const contactSection = document.getElementById('contact');
+const portfolioDate = document.querySelector('.portfolio-date');
+const domProjectsList = document.querySelector('.projects-list');
+const projectsImage = document.querySelector('.projects-image');
+
+function clickLink(idx){
+    if(idx === 0){
+        window.scrollTo(0, heroSection.offsetTop)
+    }else if(idx === 1){
+        window.scrollTo(0, projectsSection.offsetTop)
+    }else if(idx === 2){
+        window.scrollTo(0, aboutSection.offsetTop)
+    }else if(idx === 3){
+        window.scrollTo(0, contactSection.offsetTop)
+    }
+    // window.scrollTo(0, idx * window.innerHeight);
+}
+
+portfolioDate.innerText = `Portfolio ${new Date().getFullYear().toString().substring(2,4)}`;
+
+
+// Project image on hover settings
+domProjectsList.addEventListener('mouseenter', displayImage);
+domProjectsList.addEventListener('mouseleave', displayImage);
+
+function displayImage(e){
+    if(e.type === 'mouseenter'){
+        projectsImage.style.opacity = 1;
+    }else{
+        projectsImage.style.opacity = 0;
+    }
+}
+
+// Append projects to list in Projects section.
+projectsList.forEach(project => {
+    let li = document.createElement('li');
+    let projectName = document.createElement('h4');
+    projectName.innerText = project.name;
+    li.addEventListener('mouseenter', () => {
+        projectsImage.style.backgroundImage = `url(${project.image})`;
+    });
+ 
+    li.appendChild(projectName);
+    domProjectsList.appendChild(li)
+})
+
+
+function lerp(start, end, t){
+    return start * (1 - t) + end * t;
+};
+
+function setDimensions(){
+
+    // First we get the viewport height and we multiple it by 1% to get a value for a vh unit
+    let vh = window.innerHeight * 0.01;
+    // Then we set the value in the --vh custom property to the root of the document
+    document.documentElement.style.setProperty('--vh', `${vh}px`);
+    document.body.style.height = `${scrollable.getBoundingClientRect().height}px`
+}
+
+// Page scroll variables
+let current = 0;
+let target = 0;
+let ease = 0.075;
+
+// Projects image variables
+let currentX = 0;
+let currentY = 0;
+let targetX = 0;
+let targetY = 0;
+
+window.addEventListener('mousemove', (e) => {
+    targetX =e.clientX;
+    targetY = e.clientY;
+})
+
+function smoothScroll(){
+
+    // Scroll
+    target = window.scrollY;
+    current = lerp(current, target, ease);
+
+    // projects image
+    currentX = lerp(currentX, targetX, 0.055).toFixed(2);
+    currentY = lerp(currentY, targetY, 0.055).toFixed(2);
+
+    scrollable.style.transform = `translate3d(-50%, -${current}px, 0)`;
+    projectsImage.style.transform = `translate3d(${currentX - 125}px, ${currentY - 75}px, 0)`;
+
+    requestAnimationFrame(smoothScroll)
+}
+
+
+
+
+setDimensions();
+window.addEventListener('resize', setDimensions)
+smoothScroll()
+
+
+
---
+++ projects.js
@@ -0,0 +1,35 @@
+
+const projectsList = [
+    {
+        id: 0,
+        name: "Memo Board",
+        image: "https://images.unsplash.com/photo-1627386172141-c6fec8bf5aa3?ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwyfHx8ZW58MHx8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1000&q=60"
+    },
+    {
+        id: 1,
+        name: "Blog",
+        image: "https://images.unsplash.com/photo-1626541439398-9f1d3038cd34?ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHw1fHx8ZW58MHx8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1000&q=60"
+    },
+    {
+        id: 2,
+        name: "Drum Sequencer",
+        image: "https://images.unsplash.com/photo-1627308098537-5f26c63570f7?ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwxMnx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1000&q=60"
+    },
+    {
+        id: 3,
+        name: "Infinite Gallery",
+        image: "https://images.unsplash.com/photo-1627401261874-6be9039d2a48?ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwyOXx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1000&q=60"
+    },
+    {
+        id: 4,
+        name: "Expanding Gallery",
+        image: "https://images.unsplash.com/photo-1617046876710-f38e8a1c4f93?ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHw0Nnx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1000&q=60"
+    },
+    {
+        id: 5,
+        name: "WebGL RGB Distortion",
+        image: "https://images.unsplash.com/photo-1627328561499-a3584d4ee4f7?ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHw0OHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1000&q=60"
+    }
+]
+
+;
