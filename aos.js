8e94a2270c6a6cd01da2a46569d4f85976d75c24
conorbailey90 <conbailey90@gmail.com>
2021-08-02 09:19:31 +0100

    Animate on scroll feature added

---
+++ aos.js
@@ -0,0 +1,40 @@
+const h1Headers = [...document.querySelectorAll('h1')];
+const h2Headers = [...document.querySelectorAll('h2')];
+const h3Headers = [...document.querySelectorAll('h3')];
+const paragraphs = [...document.querySelectorAll('p')];
+const listItems = [...document.querySelectorAll('li')];
+const socials = [...document.querySelectorAll('i')];
+const bioImage =[...document.querySelectorAll('.bio-image')];
+const tech =[...document.querySelectorAll('.tech')];
+const svg =[...document.querySelectorAll('svg')];
+
+
+let elements = [h1Headers, h2Headers, h3Headers, paragraphs, listItems, socials, bioImage, svg, tech]
+
+
+let options = {
+    rootMargin: '0px',
+    threshold: .2
+  }
+  let setItemActive = (entries, observer) => {
+    entries.forEach(entry => {
+        if(entry.isIntersecting){
+            entry.target.style.opacity = 1;
+        }else{
+            entry.target.style.opacity = 0;
+        }
+       
+    })
+}
+
+let observer = new IntersectionObserver(setItemActive, options);
+
+elements.forEach((list,idx) => {
+    list.forEach(item => {
+        observer.observe(item)
+    })
+    
+})
+
+
+
